b15bd71724a11b1df216cb7df97a2a28da056683
anshdee17 <anshdee@anshdee11.awsapps.com>
2025-07-27 00:00:08 +0000

    This is the Day1 Project

---
+++ band_name_generator.py
@@ -0,0 +1,3 @@
+pet_name = input("What's your pet name? ")  # Added space after the question mark
+city = input("What city were you born in? ")  # Added space after the question mark
+print(f"Your band name could be {pet_name} {city}")
